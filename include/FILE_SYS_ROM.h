c87b796eec7c848b441ab99d955649f9fc540d61
Harry <107435091+hazzaaclark@users.noreply.github.com>
2024-01-09 23:47:01 +0000

    added rom disk methods and further attributes

--- include/FILE_SYS_ROM.h
+++ include/FILE_SYS_ROM.h
@@ -32,7 +32,7 @@
 #else
 #define USE_ROM_BOOT
 
-typedef ATTRIBUTE_32 struct FILE_SYS_ROM
+typedef ATTRIBUTE_32 struct ROM_DISK
 {
     char* VOLUME_BASE[8];
     char* VOLUME_NAME[16];
@@ -43,7 +43,46 @@ typedef ATTRIBUTE_32 struct FILE_SYS_ROM
     U32* HEADER_OFFSET;
     U32* DATA_SIZE;
 
+    struct ROMDISK_DIR;
+
 } ROM_DISK;
+
+typedef struct ROM_FILE_HANDLER
+{
+    U32* ROM_INDEX;
+    unsigned ROM_DIR;
+    U32* INDEX_POINTER;
+    U32* ROM_SIZE;
+    ROM_DISK* MOUNT;
+
+} ROM_FILE_HANDLER;
+
+#endif
+
+#if defined(ROMDISK_PARAMS)
+#define ROMDISK_PARAMS
+#else
+#define ROMDISK_PARAMS
+
+#define     ROMDISK_ENTRY_COND       0
+#define     ROMDISK_EXIT_COND        1
+
+void ROMDISK_INIT(void);
+void ROMDISK_EXIT(void);
+
+unsigned ROMDISK_MOUNT(char* MOUNT, void* BUFFER);
+unsigned ROMDISK_UNMOUNT(char* MOUNT); 
+U32 ROMDISK_READ(void);
+U32 ROMDISK_SEEK(void);
+
+U32 ROMDISK_FIND(struct ROM_DISK* ROM, char* FUNCTION, char*);
+U32 ROMDISK_OPEN(const FS_HANDLER* FS_HANDLE, const char* FUNCTION, U32 MODE);
+U32 ROMDISK_CLOSE(void*);
+
+STATIC void ROMDISK_LIST(int*);
+
+STATIC ROM_DISK* RD_DIR_HANDLER;
+STATIC FS_HANDLER* FSH_MOUNT;
 
 #endif
 #endif
