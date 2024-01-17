7bc232c0d4ceb5922e3fc111337d2a3ebb888af0
Harry <107435091+hazzaaclark@users.noreply.github.com>
2024-01-17 15:20:19 +0000

    modularisation go brrrr

--- main.c
+++ main.c
@@ -10,6 +10,29 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+/* I'M TESTING TO DETERMINE IF MODULARISATION WOULD BENEFIT ME IN TERMS OF */
+/* REDUCING THE COMPLEXITIY OF THE MAIN FUNCTION */
+
+/* RATHER KEEP THE MAIN FUNCTION CLUTTER FREE */
+
+FILE* OPEN_FILE(const char* FILE_PATH, const char* FILE_MODE, const char* STDERR)
+{
+    /* ASSUMING THERE IS NO FILE BEING PASSED THROUGH */
+    /* CHECKS THE MODE OF THE FILE BEING PASSED THROUGH AS INPUT OR OUTPUT */
+
+    if(FILE_PATH == NULL) return FILE_MODE[0] == 'r' ? stdin : stdout;
+
+    FILE* ROM_FILE = fopen(FILE_PATH, FILE_MODE);
+
+    if(ROM_FILE == NULL)
+    {
+        fprintf(stderr, "Womp womp no file to create\n", STDERR);
+    }
+
+    return ROM_FILE;
+}
+
+
 int main(int argc, char* argv[])
 {
     char* OUTPUT_FILE_PATH;
@@ -36,6 +59,11 @@ int main(int argc, char* argv[])
         if(OUTPUT_FILE_PATH == NULL)
         {
             fprintf(stderr, "Output File Path must be specified with '-o'.\n");
+        }
+
+        else 
+        {
+            
         }
     }
 }
